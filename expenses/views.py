e2a9055bacaba43211ab9e2084a7a474c10deadb
Shubham Rathod <rathodshubham382@gmail.com>
2025-10-09 15:46:51 +0530

    Add expense list view and template

--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Expense
 
-# Create your views here.
+def expense_list(request):
+    expenses = Expense.objects.all().order_by('-date')
+    return render(request, 'expenses/expense_list.html', { 'expenses': expenses })
+

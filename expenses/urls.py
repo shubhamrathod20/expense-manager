02cbe425cb9e412a1055c583a3d085172eac3831
Shubham Rathod <rathodshubham382@gmail.com>
2025-10-09 19:46:41 +0530

    Add Edit and Delete expense functionality

--- expenses/urls.py
+++ expenses/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.expense_list, name='expense_list'),
     path('add/', views.add_expense, name='add_expense'),
+    path('edit/<int:pk>/', views.edit_expense, name='edit_expense'),
+    path('delete/<int:pk>/', views.delete_expense, name='delete_expense'),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Expense
 from .forms import ExpenseForm
 
@@ -19,3 +19,23 @@ def add_expense(request):
     else:
         form = ExpenseForm()
     return render(request, 'expenses/add_expense.html', { 'form': form })
+
+
+def edit_expense(request, pk):
+    expense = get_object_or_404(Expense, pk=pk)
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST, instance=expense)
+        if form.is_valid():
+            form.save()
+            return redirect('expense_list')
+    else:
+        form = ExpenseForm(instance=expense)
+    return render(request, 'expenses/edit_expense.html', { 'form': form, 'expense': expense })
+
+
+def delete_expense(request, pk):
+    expense = get_object_or_404(Expense, pk=pk)
+    if request.method == 'POST':
+        expense.delete()
+        return redirect('expense_list')
+    return render(request, 'expenses/confirm_delete.html', { 'expense': expense })

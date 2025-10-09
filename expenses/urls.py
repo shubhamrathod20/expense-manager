41ba836e065d992514924794a6ea0ba9c6cd7373
Shubham Rathod <rathodshubham382@gmail.com>
2025-10-09 16:18:35 +0530

    Add expense creation form and view

--- expenses/urls.py
+++ expenses/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.expense_list, name='expense_list'),
+    path('add/', views.add_expense, name='add_expense'),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Expense
+from .forms import ExpenseForm
+
 
 def expense_list(request):
     expenses = Expense.objects.all().order_by('-date')
     return render(request, 'expenses/expense_list.html', { 'expenses': expenses })
 
+
+def add_expense(request):
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            expense = form.save(commit=False)
+            expense.user = request.user
+            expense.save()
+            return redirect('expense_list')
+    else:
+        form = ExpenseForm()
+    return render(request, 'expenses/add_expense.html', { 'form': form })

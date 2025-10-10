499a156e4ecd60276cc82df46a405605d2464b67
Shubham Rathod <rathodshubham382@gmail.com>
2025-10-10 15:41:50 +0530

    Add responsive navbar with login/logout links

--- expense_manager/settings.py
+++ expense_manager/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = 'accounts/login'
-LOGIN_URL = 'accounts/login'
+LOGOUT_REDIRECT_URL = '/accounts/login'
+LOGIN_URL = '/accounts/login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
